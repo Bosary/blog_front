2fa665a8f3588c567aa20af0dbd6008e23d74190
Kevin <k.lassauniere@live.fr>
2022-10-24 09:54:59 +0200

    Add a temp Dev notes page

---
+++ src/components/pages/Dev.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Dev() {
+  return (
+    <ul className="devNotes">
+      TODO
+      <li></li>
+      <li>Finish profile section</li>
+      <li>Add a light theme</li>
+      <li>Add GIF</li>
+      <li>Add cookies to save user and likes button state through refresh</li>
+      <li>Add email verification</li>
+      <li>Add google account authentification</li>
+      <li>Add sorting ?</li>
+      <li>Add nested comments</li>
+      <li>Add notifications</li>
+      <li>Add images in comments</li>
+    </ul>
+  );
+}
