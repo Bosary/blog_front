22f571cf7ddc9d4cbb62901e03333cc7059a222e
Kevin <k.lassauniere@live.fr>
2022-10-24 09:55:33 +0200

    Add a temp Dev notes page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="Doge" />
-
       <h1>Blog</h1>
+      <span>
+        Temp: <Link to="/dev_notes">Dev Notes</Link>
+      </span>
     </header>
   );
 }
