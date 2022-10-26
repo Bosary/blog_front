82126714bf722be1dba338a160846ea1d12f1321
Kevin <k.lassauniere@live.fr>
2022-10-26 09:34:06 +0200

    Add loading state on main page

--- src/components/pages/AllPosts.js
+++ src/components/pages/AllPosts.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import env from "react-dotenv";
 
@@ -10,26 +10,37 @@ import Likes from "../Likes";
 
 export default function AllPosts() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function getPosts() {
-      const result = await axios.get(`${env.API_URL}/posts/`);
-      setPosts(result.data.posts);
+      try {
+        const result = await axios.get(`${env.API_URL}/posts/`);
+        setPosts(result.data.posts);
+        setLoading(false);
+      } catch (errors) {
+        console.log(errors);
+        navigate("/fail");
+      }
     }
     getPosts();
   }, []);
 
   return (
     <div className="container">
-      {posts.map((post) => (
-        <div className="post" key={post._id}>
-          <Author name={post.author.username} />
-          <Link to={`post/${post._id}`} state={{ id: post._id }}>
-            <Post post={post} />
-          </Link>
-          <Likes likes={post.likes} postId={post._id} />
-        </div>
-      ))}
+      {loading && <div className="loading">Loading</div>}
+      {posts &&
+        posts.map((post) => (
+          <div className="post" key={post._id}>
+            <Author name={post.author.username} />
+            <Link to={`post/${post._id}`} state={{ id: post._id }}>
+              <Post post={post} />
+            </Link>
+            <Likes likes={post.likes} postId={post._id} />
+          </div>
+        ))}
     </div>
   );
 }
